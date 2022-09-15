a6ea9f5afa5aa22823a48e95f2e063b24bfb78db
jpatryk7 <pjesionka@spartaglobal.com>
2022-09-15 15:20:21 +0100

    adjusted the code according to changes in the GetShips class and fixed some bugs with ObjectId and sata-types

--- test_get_ships.py
+++ test_get_ships.py
@@ -1,7 +1,6 @@
 import unittest
-import requests
-import random
-import pymongo
+
+import settings
 from get_ships import *
 
 
@@ -9,11 +8,11 @@ class TestGetShips(unittest.TestCase):
     def setUp(self) -> None:
         self.starships_obj = GetShips()
 
-        self.starships_json = requests.get("https://swapi.dev/api/starships/?page=1").json()
+        self.starships_json = requests.get(f"{settings.starships_url_base}?page=1").json()
         self.random_starships_index = random.randint(0, len(self.starships_json["results"]) - 1)
         self.expected_ships_random_entry = self.starships_json["results"][self.random_starships_index]
 
-        self.people_json = requests.get("https://swapi.dev/api/people/?page=1").json()
+        self.people_json = requests.get(f"{settings.people_url_base}?page=1").json()
         self.not_allowed_keys = ["height", "age"]
         self.expected_all_pilots_format = {
             "1st entry": {
@@ -28,10 +27,10 @@ class TestGetShips(unittest.TestCase):
                 "not_allowed": self.not_allowed_keys
             }
         }
-        self.connection_url = "mongodb://localhost:27017/"
-        self.db_name = "starwars"
-        self.collection_name = "characters"
-        self.db = pymongo.MongoClient("mongodb://localhost:27017/")[self.db_name]
+        self.connection_url = settings.connection_url
+        self.db_name = settings.database_name
+        self.collection_name = settings.people_collection_name
+        self.db = pymongo.MongoClient(self.connection_url)[self.db_name]
         self.db_collection = self.db[self.collection_name]
 
     def test_all_ships_length(self) -> None:
@@ -52,26 +51,24 @@ class TestGetShips(unittest.TestCase):
 
     def test_all_pilots_entry_length(self) -> None:
         expected = [3 for _ in self.starships_obj.all_pilots]
-        actual = [len(p) for p in self.starships_obj.all_pilots.items()]
+        actual = [len(p) for p in self.starships_obj.all_pilots.values()]
         self.assertEqual(expected, actual)
 
     def test_all_pilots_entry_format(self) -> None:
         expected = self.expected_all_pilots_format
         # test for each pilot
-        for pilot in self.starships_obj.all_pilots.items():
-            # test for each entry in pilot dictionary
+        for pilot in self.starships_obj.all_pilots.values():
+            # test for each entry (except the random one) in pilot dictionary
             for key, expected_val in expected.items():
-                if key == "3rd entry":
-                    self.assertNotIn(expected_val[key], pilot.keys())
-                else:
-                    self.assertIn(expected_val[key], pilot.keys())
-                    self.assertIsInstance(expected_val[key], pilot[expected_val[key]])
+                if key != "3rd entry":
+                    self.assertIn(expected_val["key"], pilot.keys())
+                    self.assertIsInstance(pilot[expected_val["key"]], expected_val["datatype"])
 
     def test__get_random_entry(self) -> None:
         doc = {"key1": 0, "key2": 0, "key3": 0, "key4": 0, "key5": 0, "key6": 0, "key7": 0}
         expected = doc.keys()
         actual = self.starships_obj._get_random_entry(doc)
-        self.assertIn(expected, actual)
+        self.assertIn(actual, expected)
 
     def test__get_random_entry_provided_not_allowed(self) -> None:
         doc = {"key1": 0, "key2": 0, "key3": 0, "key4": 0, "key5": 0, "key6": 0, "key7": 0}
@@ -79,7 +76,7 @@ class TestGetShips(unittest.TestCase):
         expected = ["key1", "key2", "key4", "key6", "key7"]
         actual = self.starships_obj._get_random_entry(doc, not_allowed)
         for _ in range(20):
-            self.assertIn(expected, actual)
+            self.assertIn(actual, expected)
 
     def test__get_pilot_id(self) -> None:
         pilot_info_entry = {
@@ -87,19 +84,20 @@ class TestGetShips(unittest.TestCase):
             "name": "Anakin Skywalker",
             "hair_color": "blond"
         }
-        expected = str(self.db_collection.find(
+        expected = ObjectId(self.db_collection.find(
             {
                 "name": "Anakin Skywalker",
                 "hair_color": "blond"
             },
             {
                 "_id": True
-            }).next())
-        actual = self.starships_obj._get_pilot_id(pilot_info_entry, self.collection_name)
+            }
+        ).next()["_id"])
+        actual = self.starships_obj._get_pilot_id(pilot_info_entry)
         self.assertEqual(expected, actual)
 
-    def test__swap_url_with_id_provided_multiple_ships(self) -> None:
-        starships = [
+    def test__swap_url_with_id(self) -> None:
+        self.starships_obj.all_ships = [
             {
                 "name": "X-wing",
                 "model": "T-65 X-wing",
@@ -130,6 +128,28 @@ class TestGetShips(unittest.TestCase):
                 "url": "https://swapi.dev/api/starships/12/"
 		    }
         ]
+        self.starships_obj.all_pilots = {
+            "https://swapi.dev/api/people/1/": {
+                "_id": "",
+                "name": "Anakin Skywalker",
+                "hair_color": str(self.db_collection.find({"name": "Anakin Skywalker"}).next()["hair_color"])
+            },
+            "https://swapi.dev/api/people/9/": {
+                "_id": "",
+                "name": "Biggs Darklighter",
+                "hair_color": str(self.db_collection.find({"name": "Biggs Darklighter"}).next()["hair_color"])
+            },
+            "https://swapi.dev/api/people/18/": {
+                "_id": "",
+                "name": "Wedge Antilles",
+                "hair_color": str(self.db_collection.find({"name": "Wedge Antilles"}).next()["hair_color"])
+            },
+            "https://swapi.dev/api/people/19/": {
+                "_id": "",
+                "name": "Jek Tono Porkins",
+                "hair_color": str(self.db_collection.find({"name": "Jek Tono Porkins"}).next()["hair_color"])
+            }
+        }
         expected = [
             {
                 "name": "X-wing",
@@ -146,10 +166,10 @@ class TestGetShips(unittest.TestCase):
                 "MGLT": "100",
                 "starship_class": "Starfighter",
                 "pilots": [
-                    str(self.db_collection.find({"name": "Anakin Skywalker"}, {"_id": True}).next()),
-                    str(self.db_collection.find({"name": "Biggs Darklighter"}, {"_id": True}).next()),
-                    str(self.db_collection.find({"name": "Wedge Antilles"}, {"_id": True}).next()),
-                    str(self.db_collection.find({"name": "Jek Tono Porkins"}, {"_id": True}).next())
+                    ObjectId(self.db_collection.find({'name': 'Anakin Skywalker'}).next()['_id']),
+                    ObjectId(self.db_collection.find({'name': 'Biggs Darklighter'}).next()['_id']),
+                    ObjectId(self.db_collection.find({'name': 'Wedge Antilles'}).next()['_id']),
+                    ObjectId(self.db_collection.find({'name': 'Jek Tono Porkins'}).next()['_id'])
                 ],
                 "films": [
                     "https://swapi.dev/api/films/1/",
@@ -161,13 +181,17 @@ class TestGetShips(unittest.TestCase):
                 "url": "https://swapi.dev/api/starships/12/"
 		    }
         ]
-        actual = self.starships_obj._swap_url_with_id(ships=starships)
+
+        self.starships_obj._swap_url_with_id(ship_index=0)
+        actual = self.starships_obj.all_ships
+
         self.assertEqual(expected, actual)
 
-    def test_save_starships_collection(self) -> None:
-        is_saved = self.starships_obj.save_starships_collection(self.collection_name)
-        self.assertTrue(is_saved)
-
-        expected = self.collection_name
+    def test_save_starships_collection_coll_name(self) -> None:
+        expected = settings.starship_collection_name
         actual = self.db.list_collection_names()
         self.assertIn(expected, actual)
+
+
+if __name__ == "__main__":
+    unittest.main()
